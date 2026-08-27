7fa01735b7b7cdeb60b29759a87719234c99d729
Thomas <thomashii@dukefirehawk.com>
2026-08-28 07:02:56 +0800

    chore: Update test cases

--- _tests/test/common/directives/for_test.dart
+++ _tests/test/common/directives/for_test.dart
@@ -6,7 +6,7 @@ import 'dart:async';
 import 'package:test/test.dart';
 import 'package:kelicap/kelicap.dart';
 
-import '../../../../packages/kelicap_test/lib/kelicap_test.dart';
+import 'package:kelicap_test/kelicap_test.dart';
 
 import 'package:_tests/matchers.dart';
 
